d8c21f84bdde1c7ee125e8835450c2104a686a08
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2026-02-12 18:06:18 +0530

    Write server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+from fastmcp import FastMCP
+from file_analysis_tools import read_text_file
+
+mcp = FastMCP("File Analysis MCP Tool")
+
+@mcp.tool()
+def read_txt(filepath :str):
+    """Read contents of a text file"""
+    return read_text_file(filepath)
+
+
+
+
+
+if __name__ == "__main__":
+    mcp.run(transport= 'http', host = "127.0.0.1")
