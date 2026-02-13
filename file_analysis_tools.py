ff4519e0247e1f8b5644bc62882120d704dd72fd
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2026-02-13 12:35:33 +0530

    Write read excel function in file_analysis_tools.py

--- file_analysis_tools.py
+++ file_analysis_tools.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+from pathlib import Path
 
 # Read txt files
 def read_text_file(filepath: str):
@@ -7,4 +8,17 @@ def read_text_file(filepath: str):
     with open(filepath, 'r') as f:
         return f.read()
 
+# Read excel files
 
+def read_xl_file(filepath: str):
+    if not filepath:
+        return f"File not found: {filepath}"
+    path = Path(filepath)
+
+    if not path.exists():
+        return f"File not found: {filepath}"
+
+    sheet_name = path.stem
+    df = pd.read_excel(path, sheet_name= sheet_name)
+    df = df.dropna(how='all')
+    return df.to_dict(orient= "records")
